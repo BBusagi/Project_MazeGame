3220811a9b3e39f5e6e778623396cd64f7f32cfb
Riku <lz354000575@gmail.com>
2023-11-09 03:14:01 +0900

    show player_point

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 迷宫地图
+# cus
 maze = [
     ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
     ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
@@ -9,11 +9,16 @@ maze = [
     ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
 ]
 
-def print_maze(maze):
-    for row in maze:
-        for cell in row:
-            print(cell, end='')
+player_point = [1, 1]  # Amn, init
+
+def print_maze(maze, player_point):
+    for m, row in enumerate(maze):
+        for n, cell in enumerate(row):
+            if [m, n] == player_point:
+                print('S', end='')
+            else:
+                print(cell, end='') #point1
         print()
 
 
-print_maze(maze)
+print_maze(maze, player_point)
